6b9f6c0ebd8aa0539ccad2d5a5e9791b058a5ab5
QuanLiang <2308459964@qq.com>
2022-03-02 20:02:00 +0800

    first commit

---
+++ src/main/java/com/quanliang/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.quanliang.git;
+
+/**
+ * @Author: xiaoBY
+ * @Date: 2022/3/2 19:54
+ */
+@SuppressWarnings("all")
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
